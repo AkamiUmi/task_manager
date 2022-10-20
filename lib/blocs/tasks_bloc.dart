cf9214da47e3c49e0b155e1a35a89715def11e0f
Akami Umi <akamiumi@gmail.com>
2022-10-20 12:55:53 +0500

    adding and deleting tasks

---
+++ lib/blocs/tasks_bloc.dart
@@ -0,0 +1,56 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:meta/meta.dart';
+
+import '../models/task.dart';
+
+part 'tasks_event.dart';
+
+part 'tasks_state.dart';
+
+class TasksBloc extends HydratedBloc<TasksEvent, TasksState> {
+  TasksBloc() : super(const TasksState()) {
+    on<AddTask>(_onAddTask);
+    on<UpdateTask>(_onUpdateTask);
+    on<DeleteTask>(_onDeleteTask);
+  }
+
+  void _onAddTask(AddTask event, Emitter<TasksState> emit) {
+    final state = this.state;
+    emit(TasksState(
+      allTasks: List.from(state.allTasks)..add(event.task),
+    ));
+  }
+
+  void _onUpdateTask(UpdateTask event, Emitter<TasksState> emit) {
+    final state = this.state;
+    final task = event.task;
+
+    List<Task> allTask = List.from(state.allTasks)..remove(task);
+    task.isDone == false
+        ? allTask.add(task.copyWith(isDone: true))
+        : allTask.add(task.copyWith(isDone: false));
+
+    emit(TasksState(allTasks: allTask));
+  }
+
+  void _onDeleteTask(DeleteTask event, Emitter<TasksState> emit) {
+    final state = this.state;
+    emit(TasksState(
+      allTasks: List.from(state.allTasks)..remove(event.task),
+    ));
+  }
+
+  @override
+  TasksState? fromJson(Map<String, dynamic> json) {
+    return TasksState.fromMap(json);
+  }
+
+  @override
+  Map<String, dynamic>? toJson(TasksState state) {
+    return state.toMap();
+  }
+}
